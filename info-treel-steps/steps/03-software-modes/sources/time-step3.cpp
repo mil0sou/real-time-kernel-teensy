acaf0357505569f4664017fa8f947d5b156123b1
Milo <122978724+mil0sou@users.noreply.github.com>
2025-05-06 09:17:14 +0200

    ajout mais bon

---
+++ info-treel-steps/steps/03-software-modes/sources/time-step3.cpp
@@ -0,0 +1,24 @@
+#include "all-headers.h"
+
+//-----------------------------------------------------------------------------
+//   Configure systick
+//-----------------------------------------------------------------------------
+
+void startSystick (USER_MODE) {
+//------------------------------------ Configure Systick
+  SYST_RVR = CPU_MHZ * 1000 - 1 ; // Underflow every ms
+  SYST_CVR = 0 ;
+  SYST_CSR = SYST_CSR_CLKSOURCE | SYST_CSR_ENABLE ;
+}
+
+//-----------------------------------------------------------------------------
+//   busyWaitDuring
+//-----------------------------------------------------------------------------
+
+void busyWaitDuring (USER_MODE_ const uint32_t inDelayMS) {
+  for (uint32_t i=0 ; i<inDelayMS ; i++) {
+    while ((SYST_CSR & SYST_CSR_COUNTFLAG) == 0) {} // Busy wait, polling COUNTFLAG
+  }
+}
+
+//-----------------------------------------------------------------------------
